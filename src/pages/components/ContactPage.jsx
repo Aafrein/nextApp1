0d93fabdf2605a331cc3229431e54ce40c033556
Aafrein <aafreinrafiq11@gmail.com>
2024-03-29 17:15:21 +0530

    changes

--- src/pages/components/ContactPage.jsx
+++ src/pages/components/ContactPage.jsx
@@ -2,24 +2,23 @@ import React from "react";
 
 function ContactPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-yellow-400">
+    <div className="flex flex-col items-center justify-center h-screen bg-pink-600">
       <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
       <p className="text-lg mb-8">
-        We would love to hear from you! Reach out to us using the contact
-        information below please:
+        Contact us using the Information given below:
       </p>
 
       <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold mb-4">Contact Information</h2>
         <ul>
           <li className="flex items-center mb-2">
-            <span>123 Main Street, City, Country</span>
+            <span>123 Main Street, City, Country side</span>
           </li>
           <li className="flex items-center mb-2">
-            <span>+1234567890</span>
+            <span>+7986798678</span>
           </li>
           <li className="flex items-center">
-            <span>example@example.com</span>
+            <span>aafrein@example.com</span>
           </li>
         </ul>
       </div>
